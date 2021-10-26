0d9efc8f958f1af7503323b330ba87e5da0ab642
Mamadou <mamadou.sall@42c.fr>
2021-10-26 22:37:19 +0200

    change image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Bienvenue à Kebab Estiam</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> Choix du pain </h2>
       </header>
+      <div className="content">
+        <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq9bzz9_8GHg65mRmFsykO69A3etl36THNRg&usqp=CAU"/>
+        <p>Baguette</p>
+      </div>
+      <div className="content">
+        <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM3gHTi2IcS8C4021h8LI2qSs7qV-5RN0_6w&usqp=CAU"/>
+        <p>Tortilla</p>
+      </div>
+      <div className="content">
+        <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1_F_5YPR4jZ_G9KuERZVZkO45SBpmc-qGIQ&usqp=CAU"/>
+        <p> pain normal </p>
+      </div>
     </div>
   );
 }
